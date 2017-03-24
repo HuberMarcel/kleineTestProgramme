c185f1d2436e8a435044d5a0fde37b11cb9425de
viona25 <viona25@r302-10.daatr16.local>
2017-03-24 15:19:36 +0100

    In GgTRekursivAufruf in Zeile 41ff das TODO fertigmachen:
            TODO: den gelesenen String readStr in die Form des args[] bringen

--- src/de/marcelhuber/aufrufvonaussen/GgTRekursivaufruf.java
+++ src/de/marcelhuber/aufrufvonaussen/GgTRekursivaufruf.java
@@ -1,6 +1,8 @@
 package de.marcelhuber.aufrufvonaussen;
 
 import de.marcelhuber.kleinetestprogramme.*;
+import java.io.*;
+import java.util.*;
 
 /**
  *
@@ -8,30 +10,52 @@ import de.marcelhuber.kleinetestprogramme.*;
  */
 public class GgTRekursivaufruf {
 
+    static int counter;
     static int[] argsInt, argsIntDurchGgT;
     static int ggT;
 
     public static void main(String[] args) {
         argsInt = new int[args.length];
         argsIntDurchGgT = new int[argsInt.length];
-        for (int i = 0; i < argsInt.length; i++) {
-            System.out.println(args[i]);
-            try {
-                argsInt[i] = Integer.parseInt(args[i]);
-            } catch (NumberFormatException ex) {
-                System.out.println("Fehlermeldung: " + ex);
-                System.out.println("Übergeben Sie ein Feld von Integer-Werten!");
-                return;
-            }
+        try {
+            argsInt = parseArgsToArgsInt(args);
+        } catch (NumberFormatException ex) {
+            System.out.println("PROGRAMM-ABBRUCH!!");
+            return;
         }
-        ggT = new GgTRekursivaufruf().go(argsInt);
+        GgTRekursivaufruf hilfsObjekt = new GgTRekursivaufruf();
+        ggT = hilfsObjekt.go(argsInt);
         if (ggT != 0) {
             for (int k = 0; k < argsIntDurchGgT.length; k++) {
                 argsIntDurchGgT[k] = argsInt[k] / ggT;
+                System.out.print(argsIntDurchGgT[k] + " ;");
             }
         }
         System.out.println("Die Ausgabe des Feldes nach Division durch den ggT!\n"
-                + GgTRekursivaufruf.toStringWerte(argsIntDurchGgT));
+                + toStringWerte(argsIntDurchGgT));
+        String readStr;
+        int strVonInts;
+        System.out.println("Ihre Eingabe neuer Werte zur Berechnung des ggT's "
+                + "eben dieser: ");
+        readStr = readString();
+//        TODO: den gelesenen String readStr in die Form des args[] bringen
+//        hier wollen wir den obigen String in ein Feld von Strings umwandeln
+        List<String> liste = new ArrayList<>();
+//        hier werden wir später mit der ArrayList-liste ein entsprechendes Array
+//        anlegen
+        String[] str = new String[1];
+        try {
+            parseArgsToArgsInt(str);
+        } catch (NumberFormatException nFex) {
+            System.out.println("Geben Sie vernünftige Zahlen ein!");
+            System.out.println("Fehlermeldung:");
+            System.out.println(nFex);
+            return;
+        };
+        hilfsObjekt.go(argsInt);
+//        System.out.println("Ihre Eingabe einer Zahl: ");
+//        strVonInts = readInt();
+//        System.out.println(strVonInts);
     }
 
     private int go(int... argsInt) {
@@ -59,6 +83,7 @@ public class GgTRekursivaufruf {
     }
 
     private static String toStringWerte(int[] x) {
+        System.out.println("counter:" + counter++);
         String str = "";
         for (int i = 0; i < x.length; i++) {
             str += "" + x[i];
@@ -69,5 +94,36 @@ public class GgTRekursivaufruf {
             }
         }
         return str;
+    }
+
+    static String readString() {
+        // Mit Kanonen auf Spatzen schiessen
+//        return new Scanner(System.in).nextLine();
+
+        // effizienter:
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            return br.readLine();
+        } catch (IOException ex) {
+            return "";
+        }
+    }
+
+    static int readInt() {
+        try {
+            return Integer.parseInt(readString());
+        } catch (NumberFormatException ex) {
+            return 0;
+        }
+    }
+
+    static int[] parseArgsToArgsInt(String... args) throws NumberFormatException {
+        System.out.println("parseArgsAufruf mit args.length" + args.length);
+        argsInt = new int[args.length];
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+            argsInt[i] = Integer.parseInt(args[i]);
+        }
+        return argsInt;
     }
 }
