80d3209ae2af507bc131522f03ccad594430ef70
Marcel <Marcel@Marcel_Windows8>
2017-04-12 10:39:32 +0200

    Die Methode der schriftlichen Division nochmal aktualisiert

--- src/de/marcelhuber/kryptographie/Teilbarkeit.java
+++ src/de/marcelhuber/kryptographie/Teilbarkeit.java
@@ -9,6 +9,9 @@ import de.marcelhuber.systemtools.PressEnter;
  */
 public class Teilbarkeit {
 
+    boolean showCalculation = true;
+    String divisionszeichen = ":";
+
     public static void main(String[] args) {
         boolean assertionEnabled = false;
         assert assertionEnabled = true;
@@ -18,8 +21,8 @@ public class Teilbarkeit {
             System.out.println("Assertions Disabled");
         }
 //        new Teilbarkeit().goTestFallNaiverTeilbarkeitsTest();
-//        new Teilbarkeit().goTestSchriftlicheDivision();
-        new Teilbarkeit().go();
+        new Teilbarkeit().goTestSchriftlicheDivision();
+//        new Teilbarkeit().go();
     }
 
     private void go() {
@@ -133,24 +136,29 @@ public class Teilbarkeit {
     }
 
     private void goTestSchriftlicheDivision() {
-        long a = 75;
-        long b = 0;
+        long a = 4675;
+        long b = 22;
         long[] quotientRest;
         quotientRest = simuliereSchriftlicheDivision(a, b);
-        System.out.println("Der Quotient von " + a + " durch "
-                + b + ": " + quotientRest[0]);
-        System.out.println("Der Rest der Division von " + a + " durch "
-                + b + " ist " + quotientRest[1]);
-        System.out.println("Es gilt also " + a + " = " + quotientRest[0] + "*" + b + "+"
-                + quotientRest[1]);
-        System.out.println("Kontrolle: " + quotientRest[0] + "*" + b + "+"
-                + quotientRest[1] + " = " + (quotientRest[0] * b + quotientRest[1]));
+        boolean ergebnisKontrolleLocal = false;
+        if (ergebnisKontrolleLocal) {
+
+            System.out.println("\nDer Quotient von " + a + " durch "
+                    + b + ": " + quotientRest[0]);
+            System.out.println("Der Rest der Division von " + a + " durch "
+                    + b + " ist " + quotientRest[1]);
+            System.out.println("Es gilt also " + a + " = " + quotientRest[0] + "*" + b + "+"
+                    + quotientRest[1]);
+            System.out.println("Kontrolle: " + quotientRest[0] + "*" + b + "+"
+                    + quotientRest[1] + " = " + (quotientRest[0] * b + quotientRest[1]));
+        }
     }
 
     private long[] simuliereSchriftlicheDivision(long a, long b) {
         long q = 0;
         long r = 0;
         long aAnteil = 0;
+        long counter = 0;
         int verbrauchteZiffern = 0;
         a = Math.abs(a);
         b = Math.abs(b);
@@ -174,19 +182,53 @@ public class Teilbarkeit {
 //            System.out.println(("" + a).length());
 //            System.out.println(verbrauchteZiffern);
             while (verbrauchteZiffern <= ("" + a).length()) {
+                if (showCalculation) {
+                    if (counter == 0) {
+                        System.out.println("Aufgabe: " + a + divisionszeichen + b
+                                + " = Ganz-Zahl" + "+" + "Rest");
+                    }
+//                    System.out.println("\nSchritt " + ++counter + "(Teil-Division): "
+//                            + "     " + aAnteil+ divisionszeichen + b + "=" + (aAnteil / b));
+                    System.out.printf("%nSchritt " + ++counter + " (Teil-Division): %1$20d"
+                            + divisionszeichen + "%2$d = %3$d%n", aAnteil, b, (aAnteil / b));
+                }
                 ergebnisZahl.append("" + aAnteil / b);
-//                System.out.println(ergebnisZahl);
+                if (showCalculation) {
+//                            + aAnteil + "-" + (aAnteil / b) + "*" + b + "=" + (aAnteil % b));
+//                    System.out.println("Schritt " + counter++ + "(Zurückrechnen):    "
+                    System.out.printf("Schritt " + counter++ + " (Zurückrechnen): "
+                            + "%1$18d-%2$d*%3$d = %4$d%n", aAnteil,
+                            (aAnteil / b), b, (aAnteil % b));
+                }
                 aAnteil = aAnteil % b;
 //                System.out.println(verbrauchteZiffern);
                 if (verbrauchteZiffern < ("" + a).length()) {
                     aAnteil = Long.parseLong(
-                            "" + aAnteil + ("" + a).substring(verbrauchteZiffern, ++verbrauchteZiffern));
+                            "" + aAnteil + ("" + a).substring(verbrauchteZiffern,
+                                    ++verbrauchteZiffern));
                 } else {
                     verbrauchteZiffern++;
                 }
 //                System.out.println("ergebnisZahl:" + ergebnisZahl);
                 r = aAnteil;
-//                System.out.println(r);
+                if (showCalculation) {
+                    System.out.print("Nächste Zahl nach dem Zurückrechnen = "
+                            + r);
+                    if (verbrauchteZiffern <= ("" + a).length()) {
+                        System.out.println(" (Ziffer " + ("" + a).substring(verbrauchteZiffern - 1,
+                                verbrauchteZiffern) + " der Zahl " + a
+                                + " an die zuletzte zurückgerechnete Zahl angehängt)");
+                    } else {
+                        System.out.println("");
+                    }
+                    System.out.println("Aktuelle Darstellung der Ganz-Zahl  = "
+                            + ergebnisZahl);
+                }
+            }
+            if (showCalculation) {
+                System.out.println("\nEs gilt also:");
+                System.out.println(a + divisionszeichen + b + " = "
+                        + ergebnisZahl + " REST " + r);
             }
         }
         long[] rueckgabeQuotientRest = {Long.parseLong(ergebnisZahl.toString()), r};
