c36c39010a2d9e10cf7d86854efc3aea1f9be998
Marcel <Marcel@Marcel_Windows8>
2017-05-11 11:07:15 +0200

    Kleine Änderungen/Ergänzungen bei PublicStaticVoidMainDemo

--- src/de/marcelhuber/pruefungsvorbereitung/oca/PublicStaticVoidMainDemo.java
+++ src/de/marcelhuber/pruefungsvorbereitung/oca/PublicStaticVoidMainDemo.java
@@ -10,11 +10,22 @@ public class PublicStaticVoidMainDemo {
     public static void main(String[] args) {
         int x = 4;
         int y = 22;
-        int j = y += x += 2;     // von rechts nach links abgearbeitet
+        int j = y += x += 2;     // hier von rechts nach links abgearbeitet
         System.out.println("x: " + x + ", y: " + y + ", j: " + j);
         // x: 6, y: 28 , j: 28
         AnotherPublicStaticVoidMain.main(new String[]{"Hallo!!", "Liebe Welt!"});
         ChildOfPublicStaticVoidMain.main(new String[]{"Hallo!!", "Coole Welt!"});
+        // In der Prüfung stand vielleicht auch sowas da:
+//         AnotherPublicStaticVoidMain.main({"Hallo!!", "Liebe Welt!"});
+        AndAnotherPublicStaticVoidMain.main("Hallo ", "Welt");
+//        AndAnotherPublicStaticVoidMain.main({"Hallo ", "Welt"});       // das geht so nicht
+    }
+}
+
+class AndAnotherPublicStaticVoidMain {
+
+    public static void main(String... args) {
+        System.out.println(args[0] + "" + args[1]);
     }
 }
 
@@ -25,7 +36,7 @@ class AnotherPublicStaticVoidMain {
     }
 }
 
-class ChildOfPublicStaticVoidMain {
+class ChildOfPublicStaticVoidMain extends PublicStaticVoidMainDemo {
 
     public static void main(String[] args) {
         System.out.println("Ich bin eine Kindklasse!");
