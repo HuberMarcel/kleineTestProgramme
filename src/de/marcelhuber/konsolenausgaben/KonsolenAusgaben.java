9c342b78d41eaff1d7f22a550ac3fea72fe44c71
Marcel <Marcel@Marcel_Windows8>
2017-06-14 01:21:42 +0200

    Primfaktorzerlegung einer Zahl, wenn man die PFZ einer Zahl Q kennt mit Zahl | Q

---
+++ src/de/marcelhuber/konsolenausgaben/KonsolenAusgaben.java
@@ -0,0 +1,22 @@
+package de.marcelhuber.konsolenausgaben;
+// dieses Programm auch mal auf der Windows-Konsole ausführen!!
+//
+// java de.marcelhuber.konsolenausgaben.KonsolenAusgaben vom classes-Ordner
+// ausgehend aufrufen
+
+import java.io.Console;
+
+/**
+ *
+ * @author Marcel Huber
+ */
+public class KonsolenAusgaben {
+
+    public static void main(String[] args) {
+        System.out.println("Wir testen hier Umlaute etc: ÄÖÜäöüß...");
+        Console cons = System.console();
+        if (!(cons == null)) {
+            cons.printf("Wir testen hier Umlaute etc: ÄÖÜäöüß...%n");
+        }
+    }
+}
