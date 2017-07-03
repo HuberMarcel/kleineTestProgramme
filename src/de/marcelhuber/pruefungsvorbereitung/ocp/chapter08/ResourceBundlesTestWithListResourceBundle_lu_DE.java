bd1a30d5cf8d675799dd655b647826d9746c8017
Marcel <Marcel@Marcel_Windows8>
2017-07-03 10:07:51 +0200

    Änderungen bei Resource-Bundles... dran denken: Dort benötigt man IMMER den voll qualifizierten "Klassen-"name des Bundles

--- src/de/marcelhuber/pruefungsvorbereitung/ocp/chapter08/ResourceBundlesTestWithListResourceBundle_lu_DE.java
+++ src/de/marcelhuber/pruefungsvorbereitung/ocp/chapter08/ResourceBundlesTestWithListResourceBundle_lu_DE.java
@@ -1,9 +1,7 @@
 package de.marcelhuber.pruefungsvorbereitung.ocp.chapter08;
 
 import de.marcelhuber.systemtools.Pause;
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.ListResourceBundle;
 import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
@@ -12,22 +10,37 @@ import java.util.ResourceBundle;
  *
  * @author Marcel Huber
  */
-public class ResourceBundles {
-    // wichtig: die properties-Files werden quasi wie Java Quellcode gehändelt
-    //          sie müssen also kompiliert werden etc., jedenfalls, wenn ich das
-    //          richtig verstanden habe. Hier findet man sie nun in dem Package
-    //          resourcebundles, und dann im Unterordner resources 
-    //          (letzterer wird dann über resourcebundles.resources angesprochen
-    
+public class ResourceBundlesTestWithListResourceBundle_lu_DE extends ListResourceBundle {
+
+    Object[][] objectAs2DArray;
+
+    @Override
+    protected Object[][] getContents() {
+        objectAs2DArray = new Object[][]{
+            {"bitte", "wann ech glift"},
+            {"danke", "merci"},
+            {"Auf Wiedersehen, und Danke für das dumme Gespräch!",
+                "Äddi a merci fir d'domm Gespréich!"},};
+        return objectAs2DArray;
+    }
+
     public static void main(String[] args) {
-//        String whereAmI = new File("").getAbsolutePath();
+        new ResourceBundlesTestWithListResourceBundle_lu_DE().goTestThisResourceBundle();
+    }
+
+    private void goTestThisResourceBundle() {
+        //        String whereAmI = new File("").getAbsolutePath();
 //        System.out.println("In welchem Verzeichnis bin ich? Deine Antwort:");
 //        System.out.println(whereAmI);
 //        Path whereAmIPath = Paths.get(whereAmI);
 //        System.out.println(whereAmIPath);
         Locale loc = new Locale("lu", "DE");
+        loc = new Locale("lu");
+//        ResourceBundle rb
+//                = ResourceBundle.getBundle("resourcebundles.resources.Labels", loc);
         ResourceBundle rb
-                = ResourceBundle.getBundle("resourcebundles.resources.Labels", loc);
+                = ResourceBundle.getBundle(
+                        "de.marcelhuber.pruefungsvorbereitung.ocp.chapter08.ResourceBundlesTestWithListResourceBundle_lu_DE", loc); // 
         try {
             System.out.println("Wir fangen die \"MissingResourceException\" hier"
                     + "ab - allerdings Ausgabe mit System.err!");
