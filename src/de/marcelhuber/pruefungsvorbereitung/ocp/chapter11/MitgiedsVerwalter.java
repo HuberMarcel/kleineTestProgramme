9ba1e6f169493aaff04b8dbd1a0d0bf2cc982216
Marcel <Marcel@Marcel_Windows8>
2017-08-09 15:52:09 +0200

    Beispiel für eine Anwendung des Interfaces Comparable...

---
+++ src/de/marcelhuber/pruefungsvorbereitung/ocp/chapter11/MitgiedsVerwalter.java
@@ -0,0 +1,56 @@
+package de.marcelhuber.pruefungsvorbereitung.ocp.chapter11;
+
+import de.marcelhuber.systemtools.Marker;
+import java.util.TreeSet;
+
+/**
+ *
+ * @author Marcel Huber letzte Änderung: 09.08.2017
+ */
+public class MitgiedsVerwalter {
+
+    public static void main(String[] args) {
+        MitgiedsVerwalter dummyObject = new MitgiedsVerwalter();
+        dummyObject.go01();
+    }
+
+    private void go01() {
+        Mitglied marcel = new Mitglied("Huber", "Marcel", 1990);
+        Mitglied sascha = new Mitglied("Huber", "Sascha", 1988);
+        Mitglied pascal = new Mitglied("Huber", "Pascal", 1992);
+        Mitglied opa = new Mitglied("Huber", "Nikolaus", 1992);
+        Mitglied mama = new Mitglied("Huber", "Gisela", 1964);
+        Mitglied papa = new Mitglied("Huber", "Herbert", 1972);
+        Mitglied opa02 = new Mitglied("Huber", "Nikolaus", 1992);
+        Mitglied opa03 = new Mitglied("Huber", "Nikki", 1972);
+        Mitglied mamaNeu = new Mitglied("Huber", "Gisela", 1964);
+        TreeSet<Mitglied> mitglieder = new TreeSet<>();
+        System.out.println("Mama:");
+        System.out.println("Hinzufügen hat funktioniert? " + mitglieder.add(mama));
+        System.out.println("Pascal:");
+        System.out.println("Hinzufügen hat funktioniert? " + mitglieder.add(pascal));
+        System.out.println("Marcel:");
+        System.out.println("Hinzufügen hat funktioniert? " + mitglieder.add(marcel));
+        System.out.println("Sascha:");
+        System.out.println("Hinzufügen hat funktioniert? " + mitglieder.add(sascha));
+        System.out.println("Papa:");
+        System.out.println("Hinzufügen hat funktioniert? " + mitglieder.add(papa));
+        System.out.println("Mama:");
+        System.out.println("Hinzufügen hat funktioniert? " + mitglieder.add(mama));
+        System.out.println("Opa:");
+        System.out.println("Hinzufügen hat funktioniert? " + mitglieder.add(opa));
+        System.out.println("Opa02:");
+        System.out.println("Hinzufügen hat funktioniert? " + mitglieder.add(opa02));
+        System.out.println("Opa03:");
+        System.out.println("Hinzufügen hat funktioniert? " + mitglieder.add(opa03));
+        System.out.println("MamaNeu:");
+        System.out.println("Hinzufügen hat funktioniert? " + mitglieder.add(mamaNeu));
+        Marker.marker('_');
+        Marker.marker('_');
+        System.out.println("Sie sehen nun die Ausgabe der Mitglieder in der "
+                + "Reihenfolge des Eintritts in den Club:");
+        for (Mitglied mitglied : mitglieder) {
+            System.out.println(mitglied);
+        }
+    }
+}
---
+++ src/de/marcelhuber/pruefungsvorbereitung/ocp/chapter11/Mitglied.java
@@ -0,0 +1,64 @@
+package de.marcelhuber.pruefungsvorbereitung.ocp.chapter11;
+
+/**
+ *
+ * @author Marcel Huber letzte Änderung: 09.08.2017
+ */
+public class Mitglied<T extends Mitglied> implements Comparable<T> {
+
+    private String name;
+    private String vorname;
+    private long beitrittsJahr;
+
+    public Mitglied(String name, String vorname, long beitrittsjahr) {
+        this.name = name;
+        this.vorname = vorname;
+        this.beitrittsJahr = beitrittsjahr;
+    }
+
+    @Override
+    public int compareTo(T obj) {
+        if (this.beitrittsJahr < obj.getBeitrittsJahr()) {
+            return -1;
+        }
+        if (this.beitrittsJahr == obj.getBeitrittsJahr()) {
+            if (this.name == obj.getName()) {
+                return (this.vorname).compareToIgnoreCase(obj.getVorName());
+            } else {
+                return (this.name).compareToIgnoreCase(obj.getName());
+            }
+        }
+        return 1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVorName() {
+        return vorname;
+    }
+
+    public void setVorName(String vorName) {
+        this.vorname = vorName;
+    }
+
+    public long getBeitrittsJahr() {
+        return beitrittsJahr;
+    }
+
+    public void setBeitrittsJahr(long beitritsJahr) {
+        this.beitrittsJahr = beitritsJahr;
+    }
+
+    public String toString() {
+        String returnString;
+        returnString = "Name: " + name + "; Vorname: " + vorname + "; "
+                + "Beitrittsjahr: " + beitrittsJahr;
+        return returnString;
+    }
+}
