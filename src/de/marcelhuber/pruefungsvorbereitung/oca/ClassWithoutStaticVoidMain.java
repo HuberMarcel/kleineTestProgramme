692f16b8f38147933e06a5d65e3a26e4b898c3ff
Marcel <Marcel@Marcel_Windows8>
2017-08-30 13:03:30 +0200

    Test: Klasse ohne public static void main()...

---
+++ src/de/marcelhuber/pruefungsvorbereitung/oca/ClassWithoutStaticVoidMain.java
@@ -0,0 +1,17 @@
+// der Aufruf "java ClassWithoutStaticVoidMain" führt zu einem Error,
+// genauer: etwa unter 
+// J:\JAVA(DAA)__OCPJP\JAVA_OCPJP\JavaTESTs\kleineTestProgramme\src>
+// dann
+// java de.marcelhuber.pruefungsvorbereitung.oca.ClassWithoutStaticVoidMain
+// über die Konsole aufrufen, dann erscheint
+// "Fehler: Hauptklasse de.marcelhuber.pruefungsvorbereitung.oca.ClassWithoutStaticVoidMain konnte nicht 
+//          gefunden oder geladen werden"
+package de.marcelhuber.pruefungsvorbereitung.oca;
+
+/**
+ *
+ * @author Marcel
+ */
+public class ClassWithoutStaticVoidMain {
+    
+}
