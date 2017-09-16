b63dadb19294ba7ac196962b0cb116f0ae552574
Marcel <Marcel@Marcel_Windows8>
2017-09-16 14:11:57 +0200

    Package fuer Quizz der DVD des Buches angelegt

---
+++ src/de/marcelhuber/pruefungsvorbereitung/ocp/quizz/_.java
@@ -0,0 +1,23 @@
+package de.marcelhuber.pruefungsvorbereitung.ocp.quizz;
+
+import static java.lang.System.out;
+
+/**
+ *
+ * @author Marcel Huber; 16.09.2017
+ */
+public class _ {
+
+    public static void main(String[] args) {
+        args = new String[3];
+        args[0] = "-";
+        args[1] = "A";
+        args[2] = ".";
+//        String $=null;
+        String $ = "";
+        for (int x = 0; ++x < args.length; x++) {
+            $ += args[x];
+            System.out.println($);
+        }
+    }
+}
