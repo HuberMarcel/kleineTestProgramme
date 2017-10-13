c2336af1de9c893ae69e6fe5975503b3e6965665
HuberM <HuberM@PC10558.LOEWEN-GRUPPE.NET>
2017-10-13 10:25:44 +0200

    Auf Java 8 angepasst (Properties)

--- src/de/marcelhuber/mathematik/algebra/PartitionierungEinerNatuerlichenZahlQuickAndDirty.java
+++ src/de/marcelhuber/mathematik/algebra/PartitionierungEinerNatuerlichenZahlQuickAndDirty.java
@@ -19,7 +19,7 @@ public class PartitionierungEinerNatuerlichenZahlQuickAndDirty {
         partitionsListe = new ArrayList<>();
     }
 
-    private final List<Long[]> partitionsListe;
+    private List<Long[]> partitionsListe;
 
     public static void main(String[] args) {
         PartitionierungEinerNatuerlichenZahlQuickAndDirty dummyObject = new PartitionierungEinerNatuerlichenZahlQuickAndDirty();
