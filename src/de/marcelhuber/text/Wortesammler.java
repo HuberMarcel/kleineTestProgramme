ac5825bc3c939ea456e58794f3512599fe9c4a74
Marcel <Marcel@Marcel_Windows8>
2017-12-21 02:29:10 +0100

    ...

---
+++ src/de/marcelhuber/text/Wortesammler.java
@@ -0,0 +1,106 @@
+package de.marcelhuber.text;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import static java.lang.Math.floor;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ *
+ * @author Marcel Huber; Datum: 21.12.2017
+ */
+public class Wortesammler {
+
+    private BufferedReader br;
+    private FileReader fr;
+    private Path path;
+    private File file;
+    private String zeile;
+    private String[] wortArray;
+    private Set<String> woerterBuch;
+    private List<String> woerterListe = new ArrayList<>();
+    private int[] wortAuswahlZahl = new int[13];
+
+    public static void main(String[] args) {
+        Wortesammler dummyObject = new Wortesammler();
+        dummyObject.go01();
+    }
+
+    private void go01() {
+        woerterBuch = new HashSet<>();
+        path = Paths.get("H:/MyMonero(Marcel)/test.txt");
+        System.out.println(path);
+        file = new File(path.toString());
+        try {
+            fr = new FileReader(file);
+        } catch (FileNotFoundException fnfEx) {
+            fnfEx.printStackTrace();
+        }
+        br = new BufferedReader(fr);
+        try {
+            while ((zeile = br.readLine()) != null) {
+                wortArray = zeile.split(" ");
+                for (String wort : wortArray) {
+                    woerterBuch.add(wort);
+                }
+            }
+            for (String wort : woerterBuch) {
+                woerterListe.add(wort);
+            }
+            System.out.println(woerterListe);
+            int number = woerterListe.size();
+            boolean doppeltZahl = false;
+            while (!doppeltZahl) {
+                boolean akzeptiert;
+                for (int k = 0; k < wortAuswahlZahl.length; k++) {
+                    akzeptiert = false;
+                    wortAuswahlZahl[k] = (int) floor(Math.random() * number);
+                    if (!doppeltZahl) {
+                        for (int m = 0; m < k; m++) {
+                            if (wortAuswahlZahl[m] == wortAuswahlZahl[k]) {
+                                doppeltZahl = true;
+                                System.out.println("");
+                                System.out.println("Doppelte Woerter an Stellen "
+                                        + "" + (m + 1) + " und " + (k + 1) + ": "
+                                        + woerterListe.get(wortAuswahlZahl[k]));
+                                System.out.println("");
+                            }
+                        }
+                    } else {
+                        while (!akzeptiert) {
+                            akzeptiert = true;
+                            for (int m = 0; m < k; m++) {
+                                if (wortAuswahlZahl[m] == wortAuswahlZahl[k]) {
+                                    akzeptiert = false;
+                                    wortAuswahlZahl[k] = (int) floor(Math.random() * number);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            System.out.println(Arrays.toString(wortAuswahlZahl));
+            String s = "";
+            for (int k = 0; k < wortAuswahlZahl.length; k++) {
+                s += " " + woerterListe.get(wortAuswahlZahl[k]);
+                s = s.trim();
+            }
+            System.out.println("");
+            System.out.println("Doppeltes Wort vorhanden? " + doppeltZahl);
+            System.out.println("");
+            System.out.println(s);
+        } catch (IOException ioEx) {
+            ioEx.printStackTrace();
+        }
+    }
+
+}
